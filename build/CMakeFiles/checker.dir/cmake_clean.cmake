7d731fda187b11c4985ad3edae9531d94001fc34
soledad89 <wangboshine@126.com>
2015-03-24 15:57:29 +0800

    add new files

---
+++ build/CMakeFiles/checker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/checker.dir/checker.c.o"
+  "checker.pdb"
+  "checker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/checker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cube.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cube.dir/cube.c.o"
+  "cube.pdb"
+  "cube"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
